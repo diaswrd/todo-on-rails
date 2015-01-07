a301bd3581ee986183a72fc04a9b6d44a9ff86fb
William Dias <diasforsaken@gmail.com>
2015-01-07 09:10:04 -0200

    Add unit tests to task model.

--- test/models/tasks_test.rb
+++ test/models/tasks_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class TasksTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should not create a task without name" do
+      task = Task.create
+
+      assert_not task.save
+  end
+
+  test "should create a task with a true status" do
+    task = Task.create name: 'my test task'
+    task.save
+
+    assert task.status
+
+    task.destroy
+  end
+
 end
